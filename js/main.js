3a491928cc0606105cb0b3c7efa882ce792a3b74
Nathan Stirling <2254880S@student.gla.ac.uk>
2020-01-08 15:16:16 +0000

    tidy up of files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDloyd9QhqpQciShEQZDXQHJDUTdz5FnPU",
+    authDomain: "dissertation-experiment.firebaseapp.com",
+    databaseURL: "https://dissertation-experiment.firebaseio.com",
+    projectId: "dissertation-experiment",
+    storageBucket: "dissertation-experiment.appspot.com",
+    messagingSenderId: "198181183945",
+    appId: "1:198181183945:web:378269b149458c41ad4818",
+    measurementId: "G-Y7ZMXFYRXR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
