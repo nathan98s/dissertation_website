2e37443fd200fd2f4f11d51470771869b574291d
Nathan <nathan1998stirling@gmail.com>
2020-01-10 14:00:17 +0000

    added new images

--- js/show_image.js
+++ js/show_image.js
@@ -6,28 +6,50 @@ function setup() {
     }
 
     if (window.sessionStorage.getItem("images") == null) {
+
         var myImages1 = new Array();
 
         myImages1[0] = "images/images/attr_20.jpg";
         myImages1[1] = "images/images/attr_27.jpg";
-        myImages1[2] = "images/images/attr_54.jpg";
-        myImages1[3] = "images/images/attr_30.jpg";
-        // myImages1[4] = "images/images/attr_35.jpg";
-        // myImages1[6] = "images/images/attr_45.jpg";
-        // myImages1[7] = "images/images/attr_50.jpg";
-        // myImages1[8] = "images/images/attr_65.jpg";
-        // myImages1[9] = "images/images/attr_73.jpg";
-        // myImages1[10] = "images/images/attr_80.jpg";
-        // myImages1[11] = "images/images/attr_repuls_20.jpg";
-        // myImages1[12] = "images/images/attr_repuls_27.jpg";
-        // myImages1[13] = "images/images/attr_repuls_30.jpg";
-        // myImages1[14] = "images/images/attr_repuls_35.jpg";
-        // myImages1[15] = "images/images/attr_repuls_45.jpg";
-        // myImages1[16] = "images/images/attr_repuls_50.jpg";
-        // myImages1[17] = "images/images/attr_repuls_54.jpg";
-        // myImages1[18] = "images/images/attr_repuls_65.jpg";
-        // myImages1[19] = "images/images/attr_repuls_73.jpg";
-        // myImages1[20] = "images/images/attr_repuls_80.jpg";
+        myImages1[2] = "images/images/attr_30.jpg";
+        myImages1[3] = "images/images/attr_35.jpg";
+        myImages1[4] = "images/images/attr_45.jpg";
+        myImages1[6] = "images/images/attr_50.jpg";
+        myImages1[7] = "images/images/attr_65.jpg";
+        myImages1[8] = "images/images/attr_73.jpg";
+        myImages1[9] = "images/images/attr_80.jpg";
+        myImages1[10] = "images/images/attr_54.jpg";
+        myImages1[11] = "images/images/attr_repuls_20.jpg";
+        myImages1[12] = "images/images/attr_repuls_27.jpg";
+        myImages1[13] = "images/images/attr_repuls_30.jpg";
+        myImages1[14] = "images/images/attr_repuls_35.jpg";
+        myImages1[15] = "images/images/attr_repuls_45.jpg";
+        myImages1[16] = "images/images/attr_repuls_50.jpg";
+        myImages1[17] = "images/images/attr_repuls_54.jpg";
+        myImages1[18] = "images/images/attr_repuls_65.jpg";
+        myImages1[19] = "images/images/attr_repuls_73.jpg";
+        myImages1[20] = "images/images/attr_repuls_80.jpg";
+        myImages1[21] = "images/images/fr_20.jpg";
+        myImages1[22] = "images/images/fr_27.jpg";
+        myImages1[23] = "images/images/fr_30.jpg";
+        myImages1[24] = "images/images/fr_35.jpg";
+        myImages1[25] = "images/images/fr_45.jpg";
+        myImages1[26] = "images/images/fr_50.jpg";
+        myImages1[26] = "images/images/fr_54.jpg";
+        myImages1[27] = "images/images/fr_65.jpg";
+        myImages1[28] = "images/images/fr_73.jpg";
+        myImages1[29] = "images/images/fr_80.jpg";
+        myImages1[30] = "images/images/repuls_20.jpg";
+        myImages1[31] = "images/images/repuls_27.jpg";
+        myImages1[32] = "images/images/repuls_30.jpg";
+        myImages1[33] = "images/images/repuls_35.jpg";
+        myImages1[34] = "images/images/repuls_45.jpg";
+        myImages1[35] = "images/images/repuls_50.jpg";
+        myImages1[36] = "images/images/repuls_54.jpg";
+        myImages1[37] = "images/images/repuls_65.jpg";
+        myImages1[38] = "images/images/repuls_73.jpg";
+        myImages1[39] = "images/images/repuls_80.jpg";
+
 
         window.sessionStorage.setItem("images", JSON.stringify(myImages1))
     }
@@ -63,9 +85,6 @@ function storeFeedback() {
         .then(function () {
             document.getElementById("feedbackText").innerHTML = "Thank you for your feedback!";
             document.getElementById("feedbackDiv").style.display = 'none'
-
-
-
         });
 }
 
@@ -120,6 +139,7 @@ function storeAnswer() {
 
     for (var i = 0; i < radio.length; i++) {
         if (radio[i].checked) {
+            console.log(img.src)
             var name = img.src.slice(-11, -4);
             answers[name] = radio[i].value;
             console.log(answers);
